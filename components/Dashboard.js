9fe6ac25cec830f382bdc91cd6e55bffc1c42edb
Ali <87544521+lukef7fywmrp@users.noreply.github.com>
2021-11-03 09:36:50 +0400

    intial commit

---
+++ components/Dashboard.js
@@ -0,0 +1,43 @@
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import SpotifyWebApi from "spotify-web-api-node";
+import { playingTrackState } from "../atoms/playerAtom";
+import Body from "./Body";
+import Player from "./Player";
+import Right from "./Right";
+import Sidebar from "./Sidebar";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
+
+function Dashboard() {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken;
+
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  useEffect(() => {
+    setShowPlayer(true);
+  }, []);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
+
+  return (
+    <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
+      <Sidebar />
+      <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+      <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+
+      <div className="fixed bottom-0 left-0 right-0 z-50">
+        <Player accessToken={accessToken} trackUri={playingTrack.uri} />
+      </div>
+    </main>
+  );
+}
+
+export default Dashboard;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
